66ed7fe4d2d420db3fad0f9a8fb95dbde0dd10d0
Facundo <facuteggi@gmail.com>
2024-06-10 22:23:06 -0300

    nueva prueba

--- front/public/main.js
+++ front/public/main.js
@@ -129,3 +129,406 @@ if (window.location.pathname.endsWith('index-admin.html') || window.location.pat
 function logout() {
     localStorage.removeItem('loggedIn'); // Eliminar el estado de la sesión
 }
+
+
+//footer
+let mainFooter = document.querySelector(".nav-footer"),
+contactFooter = document.querySelector(".contact-footer");
+
+function eliminarElementosEnResponsive(mediaQuery) {
+    if (mediaQuery.matches) {
+        // Agregar la clase "none" si el ancho de la ventana es igual o menor a 768px
+        mainFooter.classList.add("none");
+        contactFooter.classList.add("none");
+    } else {
+        // Quitar la clase "none" si el ancho de la ventana es mayor a 768px
+        mainFooter.classList.remove("none");
+        contactFooter.classList.remove("none");
+    }
+}
+// Crear una instancia de MediaQueryList con la consulta para 768px o menos
+let mediaQuery = window.matchMedia("(max-width: 768px)");
+
+// Llamar a la función para verificar el estado inicial
+eliminarElementosEnResponsive(mediaQuery);
+
+// Agregar un listener para verificar si cambia el estado de la ventana
+mediaQuery.addListener(eliminarElementosEnResponsive);
+
+let categoryCount = 0;
+let subcategoryCount = 0;
+
+document.addEventListener("DOMContentLoaded", function() {
+    loadFromLocalStorage();
+});
+
+
+//funciones para el menu-admin 
+/*
+let categoryCount = 0;
+let subcategoryCount = 0;
+
+document.addEventListener("DOMContentLoaded", function() {
+    loadFromLocalStorage();
+});
+
+function addCategory() {
+    const categoryTitle = prompt("Nombre de la categoría:");
+    if (categoryTitle) {
+        const categoryId = `category-${categoryCount}`;
+        createCategoryElement(categoryId, categoryTitle);
+        saveCategoryToLocalStorage(categoryId, categoryTitle);
+        categoryCount++;
+    }
+}
+
+function addSubcategory(categoryId) {
+    const subcategoryTitle = prompt("Nombre de la subcategoría:");
+    if (subcategoryTitle) {
+        const subcategoryId = `subcategory-${subcategoryCount}`;
+        createSubcategoryElement(categoryId, subcategoryId, subcategoryTitle);
+        saveSubcategoryToLocalStorage(categoryId, subcategoryId, subcategoryTitle);
+        subcategoryCount++;
+    }
+}
+
+function addProduct(subcategoryId) {
+    const productName = prompt("Nombre del producto:");
+    const productDescription = prompt("Descripción del producto:");
+    const productPrice = prompt("Precio del producto:");
+    if (productName && productDescription && productPrice) {
+        createProductElement(subcategoryId, productName, productDescription, productPrice);
+        saveProductToLocalStorage(subcategoryId, productName, productDescription, productPrice);
+    }
+}
+
+function editCategory(categoryId) {
+    const categoryTitle = prompt("Nuevo nombre de la categoría:");
+    if (categoryTitle) {
+        document.querySelector(`#${categoryId} h2`).textContent = categoryTitle;
+        updateCategoryInLocalStorage(categoryId, categoryTitle);
+    }
+}
+
+function deleteCategory(categoryId) {
+    document.getElementById(categoryId).remove();
+    removeCategoryFromLocalStorage(categoryId);
+}
+
+function editSubcategory(categoryId, subcategoryId) {
+    const subcategoryTitle = prompt("Nuevo nombre de la subcategoría:");
+    if (subcategoryTitle) {
+        document.querySelector(`#${subcategoryId} h3`).textContent = subcategoryTitle;
+        updateSubcategoryInLocalStorage(categoryId, subcategoryId, subcategoryTitle);
+    }
+}
+
+function deleteSubcategory(categoryId, subcategoryId) {
+    document.getElementById(subcategoryId).remove();
+    removeSubcategoryFromLocalStorage(categoryId, subcategoryId);
+}
+
+function editProduct(subcategoryId, productIndex) {
+    const productName = prompt("Nuevo nombre del producto:");
+    const productDescription = prompt("Nueva descripción del producto:");
+    const productPrice = prompt("Nuevo precio del producto:");
+    if (productName && productDescription && productPrice) {
+        const product = { name: productName, description: productDescription, price: productPrice };
+        updateProductElement(subcategoryId, productIndex, product);
+        updateProductInLocalStorage(subcategoryId, productIndex, product);
+    }
+}
+
+function deleteProduct(subcategoryId, productIndex) {
+    document.querySelector(`#products-${subcategoryId}`).children[productIndex].remove();
+    removeProductFromLocalStorage(subcategoryId, productIndex);
+}
+
+function createCategoryElement(categoryId, categoryTitle) {
+    const carta = document.getElementById('carta');
+    const categoryDiv = document.createElement('div');
+    categoryDiv.classList.add('category');
+    categoryDiv.id = categoryId;
+
+    categoryDiv.innerHTML = `
+        <h2>${categoryTitle}</h2>
+        <button class="btn-menu" onclick="editCategory('${categoryId}')">Editar Categoría</button>
+        <button class="btn-menu" onclick="deleteCategory('${categoryId}')">Eliminar Categoría</button>
+        <button class="btn-menu" onclick="addSubcategory('${categoryId}')">Agregar Subcategoría</button>
+        <div id="subcategories-${categoryId}" class="subcategories"></div>
+    `;
+
+    carta.appendChild(categoryDiv);
+}
+
+function createSubcategoryElement(categoryId, subcategoryId, subcategoryTitle) {
+    const subcategoriesDiv = document.getElementById(`subcategories-${categoryId}`);
+    const subcategoryDiv = document.createElement('div');
+    subcategoryDiv.classList.add('subcategory');
+    subcategoryDiv.id = subcategoryId;
+
+    subcategoryDiv.innerHTML = `
+        <h3>${subcategoryTitle}</h3>
+        <button class="btn-menu" onclick="editSubcategory('${categoryId}', '${subcategoryId}')">Editar Subcategoría</button>
+        <button class="btn-menu" onclick="deleteSubcategory('${categoryId}', '${subcategoryId}')">Eliminar Subcategoría</button>
+        <button class="btn-menu" onclick="addProduct('${subcategoryId}')">Agregar Producto</button>
+        <div id="products-${subcategoryId}" class="products-row"></div>
+    `;
+
+    subcategoriesDiv.appendChild(subcategoryDiv);
+}
+
+function createProductElement(subcategoryId, productName, productDescription, productPrice, productIndex) {
+    const productsDiv = document.getElementById(`products-${subcategoryId}`);
+    const productDiv = document.createElement('div');
+    productDiv.classList.add('product');
+
+    const currentProductIndex = productIndex !== undefined ? productIndex : productsDiv.children.length;
+
+    productDiv.innerHTML = `
+        <p><strong>Nombre:</strong> ${productName}</p>
+        <p><strong>Descripción:</strong> ${productDescription}</p>
+        <p><strong>Precio:</strong> $${productPrice}</p>
+        <button class="btn-menu" onclick="editProduct('${subcategoryId}', ${currentProductIndex})">Editar Producto</button>
+        <button class="btn-menu" onclick="deleteProduct('${subcategoryId}', ${currentProductIndex})">Eliminar Producto</button>
+    `;
+
+    productsDiv.appendChild(productDiv);
+}
+
+function updateProductElement(subcategoryId, productIndex, product) {
+    const productDiv = document.querySelector(`#products-${subcategoryId}`).children[productIndex];
+    productDiv.innerHTML = `
+        <p><strong>Nombre:</strong> ${product.name}</p>
+        <p><strong>Descripción:</strong> ${product.description}</p>
+        <p><strong>Precio:</strong> $${product.price}</p>
+        <button class="btn-menu" onclick="editProduct('${subcategoryId}', ${productIndex})">Editar Producto</button>
+        <button class="btn-menu" onclick="deleteProduct('${subcategoryId}', ${productIndex})">Eliminar Producto</button>
+    `;
+}
+
+function saveCategoryToLocalStorage(categoryId, categoryTitle) {
+    const categories = JSON.parse(localStorage.getItem('categories')) || [];
+    categories.push({ id: categoryId, title: categoryTitle });
+    localStorage.setItem('categories', JSON.stringify(categories));
+}
+
+function saveSubcategoryToLocalStorage(categoryId, subcategoryId, subcategoryTitle) {
+    const subcategories = JSON.parse(localStorage.getItem('subcategories')) || {};
+    if (!subcategories[categoryId]) {
+        subcategories[categoryId] = [];
+    }
+    subcategories[categoryId].push({ id: subcategoryId, title: subcategoryTitle });
+    localStorage.setItem('subcategories', JSON.stringify(subcategories));
+}
+
+function saveProductToLocalStorage(subcategoryId, productName, productDescription, productPrice) {
+    const products = JSON.parse(localStorage.getItem('products')) || {};
+    if (!products[subcategoryId]) {
+        products[subcategoryId] = [];
+    }
+    products[subcategoryId].push({
+        name: productName,
+        description: productDescription,
+        price: productPrice
+    });
+    localStorage.setItem('products', JSON.stringify(products));
+}
+
+function updateCategoryInLocalStorage(categoryId, categoryTitle) {
+    const categories = JSON.parse(localStorage.getItem('categories'));
+    const category = categories.find(cat => cat.id === categoryId);
+    if (category) {
+        category.title = categoryTitle;
+        localStorage.setItem('categories', JSON.stringify(categories));
+    }
+}
+
+function updateSubcategoryInLocalStorage(categoryId, subcategoryId, subcategoryTitle) {
+    const subcategories = JSON.parse(localStorage.getItem('subcategories'));
+    const subcategory = subcategories[categoryId].find(sub => sub.id === subcategoryId);
+    if (subcategory) {
+        subcategory.title = subcategoryTitle;
+        localStorage.setItem('subcategories', JSON.stringify(subcategories));
+    }
+}
+
+function removeCategoryFromLocalStorage(categoryId) {
+    let categories = JSON.parse(localStorage.getItem('categories'));
+    categories = categories.filter(cat => cat.id !== categoryId);
+    localStorage.setItem('categories', JSON.stringify(categories));
+
+    const subcategories = JSON.parse(localStorage.getItem('subcategories'));
+    delete subcategories[categoryId];
+    localStorage.setItem('subcategories', JSON.stringify(subcategories));
+
+    const products = JSON.parse(localStorage.getItem('products'));
+    for (const subcategoryId in products) {
+        if (products.hasOwnProperty(subcategoryId) && subcategoryId.startsWith(categoryId)) {
+            delete products[subcategoryId];
+        }
+    }
+    localStorage.setItem('products', JSON.stringify(products));
+}
+
+function removeSubcategoryFromLocalStorage(categoryId, subcategoryId) {
+    const subcategories = JSON.parse(localStorage.getItem('subcategories'));
+    subcategories[categoryId] = subcategories[categoryId].filter(sub => sub.id !== subcategoryId);
+    localStorage.setItem('subcategories', JSON.stringify(subcategories));
+
+    const products = JSON.parse(localStorage.getItem('products'));
+    delete products[subcategoryId];
+    localStorage.setItem('products', JSON.stringify(products));
+}
+
+function updateProductInLocalStorage(subcategoryId, productIndex, product) {
+    const products = JSON.parse(localStorage.getItem('products'));
+    if (products[subcategoryId]) {
+        products[subcategoryId][productIndex] = product;
+        localStorage.setItem('products', JSON.stringify(products));
+    }
+}
+
+function removeProductFromLocalStorage(subcategoryId, productIndex) {
+    const products = JSON.parse(localStorage.getItem('products'));
+    if (products[subcategoryId]) {
+        products[subcategoryId].splice(productIndex, 1);
+        localStorage.setItem('products', JSON.stringify(products));
+    }
+}
+
+function loadFromLocalStorage() {
+    const categories = JSON.parse(localStorage.getItem('categories')) || [];
+    const subcategories = JSON.parse(localStorage.getItem('subcategories')) || {};
+    const products = JSON.parse(localStorage.getItem('products')) || {};
+
+    categories.forEach(category => {
+        createCategoryElement(category.id, category.title);
+
+        if (subcategories[category.id]) {
+            subcategories[category.id].forEach(subcategory => {
+                createSubcategoryElement(category.id, subcategory.id, subcategory.title);
+
+                if (products[subcategory.id]) {
+                    products[subcategory.id].forEach((product, index) => {
+                        createProductElement(subcategory.id, product.name, product.description, product.price, index);
+                    });
+                }
+                subcategoryCount++;
+            });
+        }
+        categoryCount++;
+    });
+}*/
+
+//funciones para el menu del cliente 
+/* 
+document.addEventListener('DOMContentLoaded', () => {
+    function productosJSON(){
+        fetch('./productos.json')
+                .then(response => response.json())
+                .then(data => {
+                    const panaderiaData = data.Bakery.Panaderia;
+                    const desayunoMerienda = data.Gourmet["Desayuno / Merienda"];
+                    const almuerzoCena = data.Gourmet["Almuerzo / Cena"];
+                    const bebidasData = data.Bebidas;
+
+                    // Panadería
+                    const panaderia = document.getElementById('panaderia');
+                    if (panaderia) {
+                        for (const categoria in panaderiaData) {
+                            panaderiaData[categoria].forEach(producto => {
+                                panaderia.innerHTML += `
+                                    <div class="card">
+                                        <img src="${producto.img}" alt="${producto.titulo}">
+                                        <h3>${producto.titulo}</h3>
+                                        <p>${producto.descripcion}</p>
+                                        <p>Precio: $${producto.precio}</p>
+                                    </div>
+                                `;
+                            });
+                        }
+                    }
+
+                    // Desayuno / Merienda
+                    const desayuno = document.getElementById('desayuno');
+                    if (desayuno) {
+                        for (const key in desayunoMerienda) {
+                            if (Array.isArray(desayunoMerienda[key])) {
+                                desayunoMerienda[key].forEach(producto => {
+                                    desayuno.innerHTML += `
+                                        <div class="card">
+                                            <img src="${producto.imagen}" alt="${producto.titulo}">
+                                            <h3>${producto.titulo}</h3>
+                                            <p>${producto.descripcion}</p>
+                                            <p>Precio: ${producto.precio}</p>
+                                        </div>
+                                    `;
+                                });
+                            } else {
+                                const producto = desayunoMerienda[key];
+                                desayuno.innerHTML += `
+                                    <div class="card">
+                                        <img src="${producto.imagen}" alt="${producto.titulo}">
+                                        <h3>${producto.titulo}</h3>
+                                        <p>${producto.descripcion}</p>
+                                        <p>Precio: ${producto.precio}</p>
+                                    </div>
+                                `;
+                            }
+                        }
+                    }
+
+                    // Almuerzo / Cena
+                    const almuerzo = document.getElementById('almuerzo');
+                    if (almuerzo) {
+                        for (const key in almuerzoCena) {
+                            if (Array.isArray(almuerzoCena[key])) {
+                                almuerzoCena[key].forEach(producto => {
+                                    almuerzo.innerHTML += `
+                                        <div class="card">
+                                            <img src="${producto.imagen}" alt="${producto.titulo}">
+                                            <h3>${producto.titulo}</h3>
+                                            <p>${producto.descripcion}</p>
+                                            <p>Precio: ${producto.precio}</p>
+                                        </div>
+                                    `;
+                                });
+                            } else {
+                                const producto = almuerzoCena[key];
+                                almuerzo.innerHTML += `
+                                    <div class="card">
+                                        <img src="${producto.imagen}" alt="${producto.titulo}">
+                                        <h3>${producto.titulo}</h3>
+                                        <p>${producto.descripcion}</p>
+                                        <p>Precio: ${producto.precio}</p>
+                                    </div>
+                                `;
+                            }
+                        }
+                    }
+
+                    // Bebidas
+                    const bebidas = document.getElementById('bebidas');
+                    if (bebidas) {
+                        for (const categoria in bebidasData) {
+                            bebidasData[categoria].forEach(producto => {
+                                bebidas.innerHTML += `
+                                    <div class="card">
+                                        <img src="${producto.imagen}" alt="${producto.titulo}">
+                                        <h3>${producto.titulo}</h3>
+                                        <p>${producto.descripcion}</p>
+                                        <p>Precio: ${producto.precio}</p>
+                                    </div>
+                                `;
+                            });
+                        }
+                    }
+                })
+                .catch(error => {
+                    console.error('Error al cargar el archivo JSON:', error);
+                });
+    }
+
+    productosJSON();
+});*/
